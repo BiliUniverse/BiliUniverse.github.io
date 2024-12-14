3b429f80757f5c4f404320592a4d1fc569259628
脆薯饼 <Virgil@nanocat.me>
2024-12-14 15:10:53 +0800

    feat: new style
    Update rspress.config.ts

--- rspress.config.ts
+++ rspress.config.ts
@@ -1,13 +1,9 @@
 import fs from 'node:fs';
 import path from 'node:path';
-import type { UrlWithStringQuery } from 'node:url';
 import { pluginChangelog } from 'rspress-plugin-changelog';
 import type { ChangelogPluginOptions } from 'rspress-plugin-changelog';
-import { pluginFontOpenSans } from 'rspress-plugin-font-open-sans';
 import pluginSitemap from 'rspress-plugin-sitemap';
 import { defineConfig } from 'rspress/config';
-
-const siteUrl = 'https://BiliUniverse.github.io';
 
 const generateChangelogParams = (items: Omit<ChangelogPluginOptions['items'][number], 'type'>[]) =>
   items.map<ChangelogPluginOptions['items'][number]>((item) => ({
@@ -22,17 +18,18 @@ export default defineConfig({
   description: '哔哩哔哩功能优化及增强解决方案',
   icon: 'https://avatars.githubusercontent.com/u/129515498?s=200&v=4',
   logo: 'https://avatars.githubusercontent.com/u/129515498?s=80&v=4',
+  logoText: 'BiliUniverse',
   // logo: {
   //   light: '/rspress-light-logo.png',
   //   dark: '/rspress-dark-logo.png',
   // },
-  globalStyles: path.resolve('./assets/styles/global.css'),
   head: [
     ['link', { ref: 'preconnect', href: '//ipolyfill.edge-byted.com' }],
     ['link', { ref: 'dns-prefetch', href: '//ipolyfill.edge-byted.com' }],
-    ['script', { src: '//ipolyfill.edge-byted.com/0.0.24/polyfill.min.js', crossorigin: '' }],
+    ['script', { src: '//ipolyfill.edge-byted.com/0.0.25/polyfill.min.js', crossorigin: '' }],
   ],
   themeConfig: {
+    lastUpdated: true,
     socialLinks: [
       { icon: 'github', mode: 'link', content: 'https://github.com/BiliUniverse' },
       {
@@ -52,30 +49,10 @@ export default defineConfig({
   markdown: {
     mdxRs: false,
     remarkPlugins: [[require('remark-github')]],
-    rehypePlugins: [
-      [
-        require('rehype-urls'),
-        (url: UrlWithStringQuery, node: any) => {
-          switch (url.host) {
-            case 't.me':
-            case 'github.com':
-              node.properties.target = '_blank';
-              break;
-            case null:
-              //console.log(url);
-              break;
-            default:
-              //console.log(url);
-              break;
-          }
-        },
-      ],
-    ],
   },
   plugins: [
-    pluginFontOpenSans(),
     pluginSitemap({
-      domain: siteUrl,
+      domain: 'https://BiliUniverse.github.io',
     }),
     pluginChangelog({
       fetchOnDev: false,
